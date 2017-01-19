fa8c6701893ad6af4cbe4d097c5ffb8e72fa8763
Christian Etter <christian.etter1@swisscom.com>
2017-01-19 19:34:54 +0100

    Added DAOFActory

--- src/dao/DAOFactory.java
+++ src/dao/DAOFactory.java
@@ -1,5 +1,47 @@
 package dao;
 
-public class DAOFactory {
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
+public class DAOFactory {
+	
+	private Connection connection;
+	private static DAOFactory instance;
+
+	public static DAOFactory getInstance() {
+		if(instance == null) {
+			instance = new DAOFactory();
+		}
+		return instance;
+	}
+	
+	private DAOFactory() {
+		if(connection == null) {
+			try {
+				connect();
+			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+			  System.out.println(e.getMessage());
+			}
+		}
+	}
+	
+	public void connect() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+    
+    Class.forName("com.mysql.jdbc.Driver").newInstance();
+    
+    MysqlDataSource ds = new MysqlDataSource();
+    ds.setServerName("localhost");
+    ds.setDatabaseName("equals1DB");
+    connection = ds.getConnection("root", "alert");
+    
+	}
+  
+  public void disconnect() {
+    try {
+        connection.close();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+  }
 }
