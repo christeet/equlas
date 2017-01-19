1f3825123970eb588767fc4354a9929a7da9001e
Sven Bolzern <sbolzu@gmail.com>
2017-01-19 16:40:54 +0100

    created eclipse project with dummy main and corrected checkstyle.

---
+++ src/bfh/equals/Main.java
@@ -0,0 +1,17 @@
+package bfh.equals;
+
+public class Main {
+
+	private static String asda="basta";
+	
+	/**
+	 * The entry-point of this program.
+	 * @param args commandline arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(asda);
+		
+	}
+
+}
