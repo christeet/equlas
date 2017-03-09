35764f266c2b91a51068bbeeebd0508f9a8df3b6
Sven Bolzern <sbolzu@gmail.com>
2017-03-09 18:15:56 +0100

    sorted CasAssistantView, not editable anymore

--- src/view/CasAssistantViewController.java
+++ src/view/CasAssistantViewController.java
@@ -1,7 +1,5 @@
 package view;
 
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.util.ArrayList;
 
 import data.Course;
@@ -10,9 +8,7 @@ import data.Person;
 import data.Rating;
 import data.UserRole;
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ListProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -22,10 +18,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.SortType;
 import javafx.scene.control.TableView;
 import javafx.scene.control.Tooltip;
-import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.util.StringConverter;
 
 public class CasAssistantViewController extends EqualsView {
 
@@ -46,9 +41,9 @@ public class CasAssistantViewController extends EqualsView {
 	
 	@FXML
 	protected void initialize() {
-		table.setEditable(true);
 		studentColumn.setCellValueFactory(d -> d.getValue().getStudentNameProperty());
-		studentColumn.setPrefWidth(200);
+		studentColumn.setSortable(true);
+		studentColumn.setSortType(SortType.ASCENDING);
 		this.data = table.getItems();
 	}
 	
@@ -60,46 +55,19 @@ public class CasAssistantViewController extends EqualsView {
 		UserRole userRole = module.getUserRole();
 		final int teacherId = model.getUserLogin().getUser().getId();
 
-		ArrayList<TableColumn<Data, Number>> tableColumns = new ArrayList<>();
+		ArrayList<TableColumn<Data, String>> tableColumns = new ArrayList<>();
 		FilteredList<Course> courses = model.getCoursesListProperty().filtered(c -> {
 			return c.getModuleId() == module.getId()
 					&& ((userRole==UserRole.TEACHER) ? (c.getTeacherId() == teacherId) : (true));
 			});
 		for(Course c : courses) {
-			TableColumn<Data, Number> courseColumn = new TableColumn<Data, Number>();
+			TableColumn<Data, String> courseColumn = new TableColumn<Data, String>();
 			Label courseHeader = new Label(c.getShortName());
 			courseHeader.setTooltip(new Tooltip(c.getName()));
 			courseColumn.setGraphic(courseHeader);
 			courseColumn.setUserData(c);
-			
             courseColumn.setCellValueFactory(d -> d.getValue().getRatingsProperty(c));
-            courseColumn.setCellFactory(TextFieldTableCell.<Data, Number>forTableColumn(new StringConverter<Number>() {
-    	        private final NumberFormat nf = NumberFormat.getNumberInstance();
-    	        
-    	        {
-    	             nf.setMaximumFractionDigits(0);
-    	             nf.setMinimumFractionDigits(0);
-    	        }
-
-    	        @Override public String toString(final Number value) {
-    	        	if(value.intValue() == -1) {
-    	        		return "";
-    	        	}
-    	            return nf.format(value);
-    	        }
-
-    	        @Override public Number fromString(final String s) {
-    	        	if(s.isEmpty()) {
-    	        		return -1;
-    	        	}
-    	            try {
-    					return nf.parse(s);
-    				} catch (ParseException e) {
-    					e.printStackTrace();
-    					return -1;
-    				} 
-    	        }
-    	    }));
+            courseColumn.setPrefWidth(100);
             tableColumns.add(courseColumn);
 		}
         table.getColumns().addAll(tableColumns);
@@ -113,20 +81,14 @@ public class CasAssistantViewController extends EqualsView {
     		data.add(row);
         }
         
+		table.getSortOrder().add(studentColumn);
+        
         table.setFixedCellSize(25);
         table.prefHeightProperty().bind(table.fixedCellSizeProperty().multiply(Bindings.size(table.getItems()).add(2.01)));
         table.minHeightProperty().bind(table.prefHeightProperty());
         table.maxHeightProperty().bind(table.prefHeightProperty());
-	}
 
-    private void setNewSuccessRate(Course course, Data data, int newSuccessRate) {
-    	System.out.format("Setting new rating %d for course %s\r\n", newSuccessRate, course.getShortName());
-    	if(newSuccessRate == -1) {
-        	controller.removeRating(data.getStudent().getId(), course.getId());
-    	} else {
-    		controller.setNewSuccessRate(data.getStudent().getId(), course.getId(), newSuccessRate);
-    	}
-    }
+	}
 	
 	@Override
 	public void dispose() {
@@ -154,13 +116,13 @@ public class CasAssistantViewController extends EqualsView {
 	    	return this.studentName;
 	    }
 	    
-	    public IntegerProperty getRatingsProperty(Course course) { 
+	    public StringProperty getRatingsProperty(Course course) { 
 	    	for(Rating r : ratings) {
 	    		if(r.getCourseId() == course.getId()) {
-	    	    	return new SimpleIntegerProperty(r.getSuccessRate());
+	    	    	return new SimpleStringProperty(String.format("%d", r.getSuccessRate()));
 	    		}
 	    	}
-	    	return new SimpleIntegerProperty(-1);
+	    	return new SimpleStringProperty("");
 	    }
 	  }
 
